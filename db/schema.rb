1df6dfc943a816c252680bfb74ba54b1975417d7
Ronaldo Abrao <ronaldo.abrao@gmail.com>
2020-12-11 12:20:51 -0300

    models created

--- db/schema.rb
+++ db/schema.rb
@@ -84,8 +84,9 @@ ActiveRecord::Schema.define(version: 2020_12_10_051619) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "name"
     t.string "role"
-    t.bigint "school_id", null: false
+    t.bigint "school_id"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
