832989c1fb9562fab0263a667991556fde713831
alessandro passos <alessandro.passos@columbia.edu>
2020-12-12 22:53:15 -0300

    reports_new criado mas com varias pendencias

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,8 +1,20 @@
 class ReportsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:new]
+
   def new
+    @report = Report.new
   end
 
   def create
+    @report = Report.new(report_params)
+    @report.user_id = current_user.id
+    authorize @report
+    if @report.save
+      # redirect_to reports_path
+      redirect_to @report, notice: 'Seu relato foi criado'
+    else
+      render :new
+    end
   end
 
   def update
@@ -18,5 +30,16 @@ class ReportsController < ApplicationController
   end
 
   def show
+  end
+
+   private
+
+  def set_report
+    @report = Report.find(params[:id])
+    authorize @report
+  end
+
+  def report_params
+    params.require(:report).permit(:description, :photo)
   end
 end
