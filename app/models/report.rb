12ef617081dfd48aecf2a4522ffe15f36d5fc106
alessandro passos <alessandro.passos@columbia.edu>
2020-12-14 18:43:18 -0300

    ajuste no model do report, pra manter :photos

--- app/models/report.rb
+++ app/models/report.rb
@@ -3,7 +3,7 @@ class Report < ApplicationRecord
   belongs_to :user
   belongs_to :school
   has_many :reviews, dependent: :destroy
-  has_one_attached :photo
+  has_many_attached :photos
   validates :description, length: { maximum: 500,
                                     too_long: "%{count} caracteres, no máximo" }
 end
